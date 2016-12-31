40cb9bcb252617a549c207193208d4ec2087174d
Adriano Souza Costa <adriano@adrianosouzacosta.com>
2016-12-31 14:22:20 -0200

    - Initial commit

---
+++ Example/Pods/Target Support Files/ASAlertViewController/ASAlertViewController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ASAlertViewController : NSObject
+@end
+@implementation PodsDummy_ASAlertViewController
+@end
---
+++ Example/Pods/Target Support Files/Pods-ASAlertViewController_Example/Pods-ASAlertViewController_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ASAlertViewController_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ASAlertViewController_Example
+@end
